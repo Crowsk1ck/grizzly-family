3bfe963a8ad4ae321a98d3806856f3e5094dc853
Crowsk1ck <crowskick4133@gmail.com>
2026-05-20 04:48:39 +0200

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,24 @@
+
+// Firebase CDN CONFIG
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyBxi9jixmJPGWodjLaNlY3oXeHvOGzQtlc",
+
+authDomain: "grizzly-6c924.firebaseapp.com",
+
+projectId: "grizzly-6c924",
+
+storageBucket: "grizzly-6c924.firebasestorage.app",
+
+messagingSenderId: "1098522296641",
+
+appId: "1:1098522296641:web:1fac71feda4e7ad9a8e1c0",
+
+measurementId: "G-GPLGFK68VD"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
