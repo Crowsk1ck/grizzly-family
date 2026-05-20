11c6e12720ad2f84e33a13af9f83cc48cd91e2e6
Crowsk1ck <crowskick4133@gmail.com>
2026-05-20 04:24:38 +0200

    Add files via upload

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,51 +2,32 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-getAuth,
-GoogleAuthProvider,
-signInWithPopup,
-onAuthStateChanged,
-signOut
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
 getFirestore,
 collection,
 addDoc,
-getDocs
+getDocs,
+query,
+orderBy
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
 apiKey: "AIzaSyBxi9jixmJPGWodjLaNlY3oXeHvOGzQtlc",
-
 authDomain: "grizzly-6c924.firebaseapp.com",
-
 projectId: "grizzly-6c924",
-
 storageBucket: "grizzly-6c924.firebasestorage.app",
-
 messagingSenderId: "1098522296641",
-
 appId: "1:1098522296641:web:1fac71feda4e7ad9a8e1c0",
-
 measurementId: "G-GPLGFK68VD"
-
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-
-export const provider = new GoogleAuthProvider();
-
 export const db = getFirestore(app);
 
 export {
-signInWithPopup,
-onAuthStateChanged,
-signOut,
 collection,
 addDoc,
-getDocs
+getDocs,
+query,
+orderBy
 };
